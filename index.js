3d9bab54beb5e49d73d64aee5bdb667cfce159d6
mkevinevans <mkevinevans@gmail.com>
2016-11-08 10:43:53 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction(callback){
+	callback()
+}
+
+function returnsANamedFunction(){
+	return function named(){}
+}
+
+function returnsAnAnonymousFunction(){
+	return function(){}}
